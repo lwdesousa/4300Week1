7dbbe1858dab405f490c7a2de4e4b2203a951fc3
Leticia Witkowsky De Sousa <lwdesousa@usf.edu>
2020-01-21 17:09:41 -0500

    Add project files.

---
+++ 4300Week1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _4300Week1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This Is ISM 4300.");
+            Console.WriteLine("Hello World");
+        }
+    }
+}
